7b21d042c0737c890fb170f89f17142f265cf7f9
Ryan <ryan.wy.wong88@gmail.com>
2018-11-22 16:01:51 -0500

    continue to add styling and reorganize components

--- src/FavoriteMovies.js
+++ src/FavoriteMovies.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import Header from './Header';
 
 class FavoriteMovies extends React.Component {
     render() {
         return (
-            <div>
-                {this.props.favoriteMovies.length === 0 ? "No Favorites Selected" :
-                    this.props.favoriteMovies.map(movie => {
-                        return (
-                            <React.Fragment key={movie.id}>
-                                <h2>{movie.title}</h2>
-                                <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
-                                <button onClick={() => this.props.removeFavorite(movie)}>Remove from Favorites</button>
-                            </React.Fragment>
-                        )
-                })}
-            </div>
+            <React.Fragment>
+                <Header location={this.props.match.path}/>
+                <div>
+                    {this.props.favoriteMovies.length === 0 ? "No Favorites Selected" :
+                        this.props.favoriteMovies.map(movie => {
+                            return (
+                                <React.Fragment key={movie.id}>
+
+                                    <h2>{movie.title}</h2>
+                                    <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
+                                    <button onClick={() => this.props.removeFavorite(movie)}>Remove from Favorites</button>
+                                </React.Fragment>
+                            )
+                    })}
+                </div>
+            </React.Fragment>
         )
     }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <header>
-            <h1>Movie Generator</h1>
-            <h2>Surf movies and build your own favorites list!</h2>
-        </header>
+        <React.Fragment>
+            {props.location === "/favorites" ? 
+                <header>
+                    <h1>Your Favorite Movies</h1>
+                </header>
+                :
+                <header>
+                    <h1>Movie Generator</h1>
+                    <h2>Surf movies and build your own favorites list!</h2>
+                </header>
+            }
+        </React.Fragment>
     )
 }
 
